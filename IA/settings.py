70f48c1f027b5fc0750f5271a726388d0107f9ab
Indiankawamura <rkawamura0483@gmail.com>
2022-09-22 13:17:22 +0900

    a

--- IA/settings.py
+++ IA/settings.py
@@ -83,17 +83,31 @@ WSGI_APPLICATION = 'IA.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'IA',
-        'USER': 'postgres',
-        'PASSWORD': '0803Kawa',
-        'HOST': 'localhost',
-        'PORT': 5432,
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'IA',
+            'USER': 'postgres',
+            'PASSWORD': '0803Kawa',
+            'HOST': 'localhost',
+            'PORT': 5432,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'saintmaur$db',
+            'USER': 'saintmaur',
+            'PASSWORD': 'stmaur1872',
+            'HOST': 'saintmaur.mysql.pythonanywhere-services.com',
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
+        }
+    }
+
 
 AUTH_USER_MODEL = 'substitute.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
