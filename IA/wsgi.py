aeb78d62550a76596a9847fea26a9e0d4ebc3fa4
Indiankawamura <rkawamura0483@gmail.com>
2022-10-26 21:45:43 +0900

    a:w

--- IA/wsgi.py
+++ IA/wsgi.py
@@ -7,9 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+
 from django.core.wsgi import get_wsgi_application
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'IA.settings'
 
+path = '/home/saintmaur/saintmaur.pythonanywhere.com'
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 application = get_wsgi_application()
