61c882595c5dd177af133751ce8e91b227b2d5e1
Indiankawamura <rkawamura0483@gmail.com>
2022-10-26 21:42:35 +0900

    a:wq

--- IA/wsgi.py
+++ IA/wsgi.py
@@ -7,10 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IA.settings')
-
-
 from django.core.wsgi import get_wsgi_application
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'IA.settings'
+
 
 application = get_wsgi_application()
