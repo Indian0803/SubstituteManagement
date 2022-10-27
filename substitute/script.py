28ea98ca558b76d0d31096ad053cb4c5ed36c479
Indiankawamura <rkawamura0483@gmail.com>
2022-10-27 12:22:58 +0900

    a

--- substitute/script.py
+++ substitute/script.py
@@ -1,9 +1,10 @@
 from django.core.mail import send_mail
+Substitute.objects.get()
 send_mail(
-                    'Substitute Request',
-                    'Teacher: ' + request.user.first_name + " " + request.user.last_name +
-                    "\nDay: "+day+"\nPeriod: "+lesson+"\nMessage: "+message,
-                    'rkawamura0483@gmail.com',
-                    ['Indiankawamura@gmail.com'],
-                    fail_silently=False,
-                )
+    'Substitute Request',
+    'Teacher: ' + request.user.first_name + " " + request.user.last_name +
+    "\nDay: "+day+"\nPeriod: "+lesson+"\nMessage: "+message,
+    'rkawamura0483@gmail.com',
+    ['Indiankawamura@gmail.com'],
+    fail_silently=False,
+)
