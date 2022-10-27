43de7a1a575b28fa4f2eeb904f145b5d6cfc16a7
Indiankawamura <rkawamura0483@gmail.com>
2022-10-27 14:59:06 +0900

    a

--- substitute/views.py
+++ substitute/views.py
@@ -193,7 +193,7 @@ def absence_report_period(request):
                 send_mail(
                     'Substitute Request',
                     'Teacher: ' + request.user.first_name + " " + request.user.last_name +
-                    "\nDay: "+request.session.get("day")+"\nPeriod: "+lesson+"\nRoom: "+ subelsson.room+"\nMessage: "+message+"\n\nIf you are available, please click this url to confirm. " +
+                    "\nDay: "+request.session.get("day")+"\nPeriod: "+lesson+"\nRoom: "+ sublesson.room+"\nMessage: "+message+"\n\nIf you are available, please click this url to confirm. " +
                     "saintmaur.pythonanywhere.com/confirm/"+str(min.id) +
                     "\nIf you are unavailable, please click this url." +
                     "saintmaur.pythonanywhere.com/deny/"+str(min.id),
